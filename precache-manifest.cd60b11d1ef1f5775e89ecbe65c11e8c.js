58f371a6107c590a76895bf828ad7d39b5148c2e
mouawia sharedi <mooawia12@windowslive.com>
2022-01-18 20:35:36 +0200

    Updates

---
+++ precache-manifest.cd60b11d1ef1f5775e89ecbe65c11e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a8453550f1a4fcefaa1fff5d9c1eb4",
+    "url": "/pizza_app/index.html"
+  },
+  {
+    "revision": "3d6bbb1f35d82c5df72d",
+    "url": "/pizza_app/static/css/main.2383d6d7.chunk.css"
+  },
+  {
+    "revision": "a3b161e68139bc35fe70",
+    "url": "/pizza_app/static/js/2.c3c2b4c2.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pizza_app/static/js/2.c3c2b4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6bbb1f35d82c5df72d",
+    "url": "/pizza_app/static/js/main.b52cf13a.chunk.js"
+  },
+  {
+    "revision": "ce1c5748e97904abef23",
+    "url": "/pizza_app/static/js/runtime-main.32009e4e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5ea35d50d66e2197aec2ba9751bd098.js"
+  "/pizza_app/precache-manifest.cd60b11d1ef1f5775e89ecbe65c11e8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pizza_app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
